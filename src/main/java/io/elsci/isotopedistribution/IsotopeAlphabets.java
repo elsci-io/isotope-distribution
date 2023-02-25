1fe7b61ffe877858e29610d6684909e4dace6276
Админ <anutapontera@gmail.com>
2023-02-25 15:22:48 +0100

    refactor IsotopeAlphabets

--- src/main/java/io/elsci/isotopedistribution/IsotopeAlphabets.java
+++ src/main/java/io/elsci/isotopedistribution/IsotopeAlphabets.java
@@ -5,59 +5,17 @@ import io.elsci.multinomial.Symbol;
 import org.openscience.cdk.config.Elements;
 import org.openscience.cdk.config.IsotopeFactory;
 import org.openscience.cdk.config.Isotopes;
-import org.openscience.cdk.interfaces.IElement;
 import org.openscience.cdk.interfaces.IIsotope;
 
 import java.io.IOException;
 import java.util.*;
 
 class IsotopeAlphabets {
-    private static final Map<Symbol, IIsotope> SYMBOL_TO_ISOTOPE = new HashMap<>(); // можно вызвать метод и вернуть мапу, сделай так со всями тремя
+    private static final Map<Symbol, IIsotope> SYMBOL_TO_ISOTOPE = new HashMap<>();
     private static final Map<IIsotope, Symbol> ISOTOPE_TO_SYMBOL = new HashMap<>();
-    // создать мапу (ключ - елемент (строка), значение - алфавит) со всеми алфатами, заполни ее используя то что в методе getAlphabet
+    private static final Map<String, Alphabet> ALPHABETS = new HashMap<>();
 
-    public static Alphabet getAlphabet(String element) {
-        IsotopeFactory isoFactory;
-        try {
-            isoFactory = Isotopes.getInstance(); // вынести в метод чтобы не повторялся код с созданием фабрики
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-
-        IIsotope[] isotopes = isoFactory.getIsotopes(element);
-        List<Double> probabilities = new ArrayList<>();
-        for (IIsotope isotope : isotopes) {
-            Double abundance = isotope.getNaturalAbundance();
-            if (!(abundance.isNaN() || abundance.isInfinite() || abundance < 1e-9))
-                probabilities.add(abundance * 0.01);
-        }
-        return new Alphabet(element, toSortedArray(probabilities)); // правильная ли сортировка?
-    }
-
-    private static void fillMaps() {
-        IsotopeFactory isoFactory;
-        try {
-            isoFactory = Isotopes.getInstance();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-
-        for (Elements element : Elements.values()) {
-            Alphabet alphabet = getAlphabet(element.symbol());
-            IIsotope[] isotopes = isoFactory.getIsotopes(element.symbol());
-            Arrays.sort(isotopes, Comparator.comparing(IIsotope::getNaturalAbundance)); // можно ли просто в обратном порядке? ->
-            // -> осмотри на документацию метода getIsotopes для гарантии (точно ли изотопы возвращаются отсортированными
-            // от наименьшей до наибольшей вероятности?)
-            for (int i = 0; i < isotopes.length; i++) {
-                Symbol symbol = alphabet.getSymbol(i);
-                SYMBOL_TO_ISOTOPE.put(symbol, isotopes[i]);
-                ISOTOPE_TO_SYMBOL.put(isotopes[i], symbol);
-            }
-        }
-
-    }
-
-    public static Symbol getSymbol(IIsotope isotope) { // точно надо?
+    public static Symbol getSymbol(IIsotope isotope) {
         if (ISOTOPE_TO_SYMBOL.isEmpty())
             fillMaps();
         return ISOTOPE_TO_SYMBOL.get(isotope);
@@ -69,10 +27,48 @@ class IsotopeAlphabets {
         return SYMBOL_TO_ISOTOPE.get(s);
     }
 
-    private static double[] toSortedArray(List<Double> probabilities) {
+    public static Alphabet getAlphabet(String elementSymbol) {
+        if (ALPHABETS.isEmpty())
+            fillMaps();
+        return ALPHABETS.get(elementSymbol);
+    }
+
+    private static void fillMaps() {
+        IsotopeFactory isoFactory;
+        try {
+            isoFactory = Isotopes.getInstance();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        for (Elements element : Elements.values()) {
+            IIsotope[] isotopes = isoFactory.getIsotopes(element.symbol());
+            Arrays.sort(isotopes, Comparator.comparing(IIsotope::getNaturalAbundance));
+            Alphabet alphabet = createAlphabet(element.symbol(), isotopes);
+            ALPHABETS.put(element.symbol(), alphabet);
+            for (int i = 0; i < isotopes.length; i++) { //можно ли создавать алфавит прямо здесь? то есть перенести сюда внутренности функции createAlphabet
+                Symbol symbol = alphabet.getSymbol(i);
+                SYMBOL_TO_ISOTOPE.put(symbol, isotopes[i]);
+                ISOTOPE_TO_SYMBOL.put(isotopes[i], symbol);
+            }
+        }
+
+    }
+
+    private static Alphabet createAlphabet(String element, IIsotope[] isotopes) {
+        List<Double> probabilities = new ArrayList<>();
+        for (IIsotope isotope : isotopes) {
+            Double abundance = isotope.getNaturalAbundance();
+            if (!(abundance.isNaN() || abundance.isInfinite() || abundance < 1e-9))
+                probabilities.add(abundance * 0.01);
+        }
+        return new Alphabet(element, toArray(probabilities));
+    }
+
+    private static double[] toArray(List<Double> probabilities) {
         double[] probArray = new double[probabilities.size()];
         for (int i = 0; i < probabilities.size(); i++)
-            probArray[probArray.length - i - 1] = probabilities.get(i);
+            probArray[i] = probabilities.get(i);
         return probArray;
     }
 
--- src/main/java/io/elsci/isotopedistribution/IsotopologueIteratorFactory.java
+++ src/main/java/io/elsci/isotopedistribution/IsotopologueIteratorFactory.java
@@ -16,11 +16,9 @@ public class IsotopologueIteratorFactory {
     public static Iterator<Isotopologue> createIsotopologueIterator(String formula) {
         IMolecularFormula molecularFormula = MolecularFormulaManipulator.getMajorIsotopeMolecularFormula(formula, DefaultChemObjectBuilder.getInstance());
         HashMap<Alphabet, Integer> hashMap = new HashMap<>();
-        // todo: ideally we want to iterate over symbols within the molecular formula. Why does molecularFormula
-        //       return isotopes at all? Is there a way to return the symbol of the element and its counts?
         for (IIsotope isos : molecularFormula.isotopes()) {
             String elementSymbol = isos.getSymbol();
-            Alphabet alphabet = IsotopeAlphabets.getAlphabet(elementSymbol); // тут будем брать алфавит с мапы, чтобы не создавать их тыщу раз
+            Alphabet alphabet = IsotopeAlphabets.getAlphabet(elementSymbol);
             hashMap.put(alphabet, molecularFormula.getIsotopeCount(isos));
         }
         return new IsotopologueIterator(WordIteratorFactory.create(new WordSpec(hashMap)));
