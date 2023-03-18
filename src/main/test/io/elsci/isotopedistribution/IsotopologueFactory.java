a643142b2eb940aba4902889abd88ab2b74ce7fa
Админ <anutapontera@gmail.com>
2023-03-18 17:25:25 +0100

    Add IsotopologueIteratorReturnsIsotopologuesOfH2O() test

--- src/main/test/io/elsci/isotopedistribution/IsotopologueFactory.java
+++ src/main/test/io/elsci/isotopedistribution/IsotopologueFactory.java
@@ -11,9 +11,8 @@ import java.util.List;
 import java.util.Map;
 
 public class IsotopologueFactory {
-    private static final double SQRT_2PI = Math.sqrt(2 * Math.PI);
 
-    public static Isotopologue createIsotopologue(String[] symbols, int[] massNumbers) {
+    public static Isotopologue createIsotopologue(String[] symbols, int[] massNumbers, int combinations) {
         try {
             if (symbols.length!=massNumbers.length)
                 throw new RuntimeException("The arrays must have same length");
@@ -36,24 +35,7 @@ public class IsotopologueFactory {
             map.put(isotope, 1);
         }
 
-
-        List<Integer> combinations = new ArrayList<>();
-        int j = -1;
-        for (int i = 0; i < massNumbers.length; i++) {
-
-            if (i!=0 && symbols[i].equals(symbols[i-1]) && massNumbers[i]==massNumbers[i-1]) {
-                combinations.set(j, combinations.get(j)+1);
-            } else {
-                combinations.add(1);
-                j++;
-            }
-        }
-
-        double numerator = logGamma(symbols.length + 1);
-        double denominator = 0;
-        for (int number : combinations)
-            denominator += logGamma(number + 1);
-        abundance *= Math.exp(numerator - denominator);
+        abundance *= combinations;
 
     return new Isotopologue(map, abundance, abundance/getMaxAbundance(symbols), mass);
     }
@@ -71,14 +53,6 @@ public class IsotopologueFactory {
             maxAbundance *= isotope.getNaturalAbundance() / 100;
         }
         return maxAbundance;
-    }
-
-    private static double logGamma(double x) {
-        double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
-        double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
-                + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
-                +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
-        return tmp + Math.log(ser * SQRT_2PI);
     }
 
 }
--- src/main/test/io/elsci/isotopedistribution/IsotopologueIteratorFactoryTest.java
+++ src/main/test/io/elsci/isotopedistribution/IsotopologueIteratorFactoryTest.java
@@ -14,8 +14,8 @@ public class IsotopologueIteratorFactoryTest {
     public void IsotopologueIteratorReturnsIsotopologuesOfHydrogen2() {
         Iterator<Isotopologue> it = IsotopologueIteratorFactory.createIsotopologueIterator("H", 1E-6);
         Isotopologue[] expectedArray = new Isotopologue[] {
-                IsotopologueFactory.createIsotopologue(new String[]{"H"}, new int[]{1}),
-                IsotopologueFactory.createIsotopologue(new String[]{"H"}, new int[]{2})
+                IsotopologueFactory.createIsotopologue(new String[]{"H"}, new int[]{1}, 1),
+                IsotopologueFactory.createIsotopologue(new String[]{"H"}, new int[]{2}, 1)
         };
         for (Isotopologue expected : expectedArray) {
             assertIsotopologueEqual(expected, it.next());
@@ -26,39 +26,39 @@ public class IsotopologueIteratorFactoryTest {
     public void IsotopologueIteratorReturnsIsotopologuesOfH2() {
         Iterator<Isotopologue> it = IsotopologueIteratorFactory.createIsotopologueIterator("H2", 1E-6);
         Isotopologue[] expectedArray = new Isotopologue[] {
-                IsotopologueFactory.createIsotopologue(new String[]{"H", "H"}, new int[]{1, 1}),
-                IsotopologueFactory.createIsotopologue(new String[]{"H", "H"}, new int[]{1, 2}),
-                IsotopologueFactory.createIsotopologue(new String[]{"H", "H"}, new int[]{2, 2})
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H"}, new int[]{1, 1}, 1),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H"}, new int[]{1, 2}, 2),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H"}, new int[]{2, 2}, 1)
         };
         for (Isotopologue expected : expectedArray) {
             assertIsotopologueEqual(expected, it.next());
         }
     }
 
-//    @Test
-//    public void IsotopologueIteratorReturnsIsotopologuesOfH2O() {
-//        Iterator<Isotopologue> it = IsotopologueIteratorFactory.createIsotopologueIterator("H2O", 1E-6);
-//        Isotopologue[] expectedArray = new Isotopologue[] {
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 1, 16}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 1, 17}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 1, 18}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 2, 16}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 2, 17}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 2, 18}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{2, 2, 16}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{2, 2, 17}),
-//                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{2, 2, 18})
-//        };
-//        for (Isotopologue expected : expectedArray) {
-//            assertIsotopologueEqual(expected, it.next());
-//        }
-//    }
+    @Test
+    public void IsotopologueIteratorReturnsIsotopologuesOfH2O() {
+        Iterator<Isotopologue> it = IsotopologueIteratorFactory.createIsotopologueIterator("H2O", 1E-6);
+        Isotopologue[] expectedArray = new Isotopologue[] {
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 1, 16}, 1),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 1, 18}, 1),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 1, 17}, 1),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 2, 16}, 2),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 2, 18}, 2),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{1, 2, 17}, 2),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{2, 2, 16}, 1),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{2, 2, 18}, 1),
+                IsotopologueFactory.createIsotopologue(new String[]{"H", "H", "O"}, new int[]{2, 2, 17}, 1)
+        };
+        for (Isotopologue expected : expectedArray) {
+            assertIsotopologueEqual(expected, it.next());
+        }
+    }
 
 
 
     @Test
     public void isotopologueIteratorReturnsIsotopologues() {
-        Iterator<Isotopologue> it = IsotopologueIteratorFactory.createIsotopologueIterator("H20", 1E-6);
+        Iterator<Isotopologue> it = IsotopologueIteratorFactory.createIsotopologueIterator("O", 1E-6);
         while (it.hasNext()) {
             System.out.println(it.next());
         }
