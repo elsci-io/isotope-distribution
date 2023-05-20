7fd914555fcbd44bc78b69e3cfa71f009ee6230d
Anna Ponomarova <anutapontera@gmail.com>
2023-05-20 18:33:30 +0200

    Refactor: don't swallow an exception

--- src/test/java/io/elsci/isotopedistribution/IsotopologueFactory.java
+++ src/test/java/io/elsci/isotopedistribution/IsotopologueFactory.java
@@ -1,6 +1,5 @@
 package io.elsci.isotopedistribution;
 
-import io.elsci.isotopedistribution.Isotopologue;
 import org.openscience.cdk.Isotope;
 import org.openscience.cdk.config.Isotopes;
 import org.openscience.cdk.interfaces.IIsotope;
@@ -12,12 +11,8 @@ import java.util.Map;
 public class IsotopologueFactory {
 
     public static Isotopologue createIsotopologue(String[] symbols, int[] massNumbers, int combinations) {
-        try {
-            if (symbols.length!=massNumbers.length)
-                throw new RuntimeException("The arrays must have same length");
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
+        if (symbols.length!=massNumbers.length)
+            throw new IllegalArgumentException("The arrays must have same length: " + symbols.length + ", " + massNumbers.length);
 
         Map<IIsotope, Integer> map = new HashMap<>();
         double abundance = 1;
@@ -33,22 +28,19 @@ public class IsotopologueFactory {
             mass += isotope.getExactMass();
             map.put(isotope, 1);
         }
-
         abundance *= combinations;
-
-    return new Isotopologue(map, abundance, abundance/getMaxAbundance(symbols), mass);
+        return new Isotopologue(map, abundance, abundance/getMaxAbundance(symbols), mass);
     }
 
     private static double getMaxAbundance(String[] symbols) {
         double maxAbundance = 1;
         for (String symbol : symbols) {
-            Isotope isotope = null;
+            Isotope isotope;
             try {
                 isotope = new Isotope(Isotopes.getInstance().getMajorIsotope(symbol));
             } catch (IOException exception) {
-                exception.printStackTrace();
+                throw new RuntimeException(exception);
             }
-            assert isotope != null;
             maxAbundance *= isotope.getNaturalAbundance() / 100;
         }
         return maxAbundance;
